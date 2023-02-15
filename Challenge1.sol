6a7d35280b9030a13d49715df8f598e7cce586d6
1.Y.6 <104085793+1Y6C0DE@users.noreply.github.com>
2023-02-15 12:37:52 +0800

    Update Challenge1.sol

--- Challenge1.sol
+++ Challenge1.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 contract Challenge1 {
     uint public product; // State Variable
-    uint storage = msg.value; // Global Variable
+    uint Storage = msg.value; // Global Variable
 
     function set(uint _multiplier, uint _multiplicand) public {
         uint multiply = multiplier * multiplicand; // Local Variable
