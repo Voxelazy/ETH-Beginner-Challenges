70d4b1ef1346ab029a1a3cb9bbc6a1d593049373
1.Y.6 <104085793+1Y6C0DE@users.noreply.github.com>
2023-02-15 12:21:01 +0800

    Challenge1.sol

--- Challenge1.sol
+++ Challenge1.sol
@@ -6,14 +6,12 @@ contract Challenge1 {
     // State Variable
     uint public foo;
 
-    // You need to send a transaction to write to a state variable.
     function set(uint _num) public {
         uint number = 100; // Local Variable
         number = foo;
         foo = _num;
     }
 
-    // You can read from a state variable without sending a transaction.
     function get() public view returns (uint) {
         return foo;
     }
